1c955898e7068ea4a357b9efb75e157a482bca38
itsmesonudutta@gmail.com <itsmesonudutta@gmail.com>
2022-06-06 11:58:39 +0530

    Second Commit

--- crud/settings.py
+++ crud/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["crudsonu.herokuapp.com", "localhost"]
-
+ALLOWED_HOSTS = ["crudsonu12.herokuapp.com", "localhost"]
 
 # Application definition
 
